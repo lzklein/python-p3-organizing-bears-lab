47416b45ade9e795998e5649d84bb9373916c263
Louis Klein <louisklein71@gmail.com>
2023-06-20 23:32:45 -0700

    test

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears(name, age, sex, color, temperament, alive)
+VALUES
+    ('Mr. Chocolate', 1, 'M', 'White', 'Yes', 0),
+    ('Rowdy', 1, 'M', 'White', 'Yes', 0),
+    ('Tabitha', 1, 'M', 'White', 'Yes', 0),
+    ('Sergeant Brown', 1, 'M', 'White', 'Yes', 0),
+    ('Melissa', 1, 'M', 'White', 'Yes', 0),
+    ('Grinch', 1, 'M', 'White', 'Yes', 0),
+    ('Wendy', 1, 'M', 'White', 'Yes', 0),
+    (NULL, 1, 'M', 'White', 'Yes', 0);
